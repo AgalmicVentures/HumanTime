0a88a1b4e6d9c58b7ad0ef36e62412f8fbce742a
Ian Hutchinson <ihutchinson@agalmicventures.com>
2019-06-08 16:21:37 -0500

    Bump version to 0.1.1 to deploy the fix

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open('README.md', 'r') as readmeFile:
 
 setuptools.setup(
     name='HumanTime',
-    version='0.1.0',
+    version='0.1.1',
     author='Ian Hutchinson',
     author_email='ihutchinson@agalmicventures.com',
     description='Time for humans in Python',
